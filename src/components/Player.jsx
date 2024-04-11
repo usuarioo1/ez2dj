925434af0eca1e52faad4a4046e7be9459f0a883
unknown <nicolasgonzalezmoller@gmail.com>
2024-04-10 22:23:00 -0400

    add

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,6 +3,69 @@ import React from 'react';
 import YoutubePlayer from 'react-player/youtube';
 import '../app/globals.css';
 
+
+canciones = [
+    {
+        cancion: "The boy",
+        url: "https://www.youtube.com/watch?v=3iJQ_mX0bno&list=RD3iJQ_mX0bno&start_radio=1"
+    },
+    {
+        cancion:"Black Market",
+        url:"https://www.youtube.com/watch?v=xlKMaVzolw0&list=RD3iJQ_mX0bno&index=2"
+    },
+    {
+        cancion:"2nd Jewel",
+        url:"https://www.youtube.com/watch?v=G8KkcPpqYPs&list=RD3iJQ_mX0bno&index=3"
+    },
+    {
+        cancion:"Back for more",
+        url:"https://www.youtube.com/watch?v=lM8YW58iwSM&list=RD3iJQ_mX0bno&index=4"
+    },
+    {
+        cancion:"",
+        url:""
+    },
+    {
+        cancion:"",
+        url:""
+    },
+
+    {
+        cancion:"",
+        url:""
+    },
+    {
+        cancion:"",
+        url:""
+    },
+    {
+        cancion:"",
+        url:""
+    },
+    {
+        cancion:"",
+        url:""
+    },
+    {
+        cancion:"",
+        url:""
+    },
+    {
+        cancion:"",
+        url:""
+    }, 
+    {
+        cancion:"",
+        url:""
+    }, 
+    {
+        cancion:"",
+        url:""
+    },
+
+
+]
+
 const Reproductor = () => {
     return (
         <div className='contenedor-reproductor'>
